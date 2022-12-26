7142d42923689bc87c79f1e6f851da245f526860
IlmamFarizan <ilmam.farizan@yahoo.com>
2022-12-26 17:33:51 +0000

    first commit

---
+++ src/main/kotlin/ie/setu/config/JavalinConfig.kt
@@ -0,0 +1,74 @@
+package ie.setu.config
+
+import io.javalin.Javalin
+import ie.setu.controllers.HealthTrackerController
+import ie.setu.domain.db.Activities.description
+import io.javalin.apibuilder.ApiBuilder.*
+import io.javalin.plugin.openapi.OpenApiOptions
+import io.javalin.plugin.openapi.OpenApiPlugin
+import io.javalin.plugin.openapi.ui.ReDocOptions
+import io.javalin.plugin.openapi.ui.SwaggerOptions
+import jdk.internal.jshell.tool.resources.version
+
+
+class JavalinConfig {
+
+
+    fun startJavalinService(): Javalin {
+
+        val app = Javalin.create().apply {
+            it.registerPlugin(getConfiguredOpenApiPlugin())
+            it.defaultContentType = "application/json"
+        }.apply {
+            exception(Exception::class.java) { e, _ -> e.printStackTrace() }
+            error(404) { ctx -> ctx.json("404 - Not Found") }
+        }.start(getHerokuAssignedPort())
+    }
+
+    fun getConfiguredOpenApiPlugin() = OpenApiPlugin(
+        OpenApiOptions(
+            ProcessHandle.Info().apply {
+                title("Health Tracker App")
+                version("1.0")
+                description("Health Tracker API")
+            }
+        ).apply {
+            path("/swagger-docs") // endpoint for OpenAPI json
+            swagger(SwaggerOptions("/swagger-ui")) // endpoint for swagger-ui
+            reDoc(ReDocOptions("/redoc")) // endpoint for redoc
+        }
+    )
+
+    private fun registerRoutes(app: Javalin) {
+        app.routes {
+            path("/api/users") {
+                get(HealthTrackerController::getAllUsers)
+                post(HealthTrackerController::addUser)
+                path("{user-id}"){
+                    get(HealthTrackerController::getUserByUserId)
+                    delete(HealthTrackerController::deleteUser)
+                    patch(HealthTrackerController::updateUser)
+                    path("activities"){
+                        get(HealthTrackerController::getActivitiesByUserId)
+                    }
+                }
+                path("/email/{email}"){
+                    get(HealthTrackerController::getUserByEmail)
+                }
+            }
+            path("/api/activities") {
+                get(HealthTrackerController::getAllActivities)
+                post(HealthTrackerController::addActivity)
+            }
+        }
+    }
+
+    private fun getHerokuAssignedPort(): Int {
+        val herokuPort = System.getenv("PORT")
+        return if (herokuPort != null) {
+            Integer.parseInt(herokuPort)
+        } else 7000
+    }
+
+
+}
